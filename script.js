394f32dbbc9f130cfce0827a0cae47533258ee3a
AmeyaDeore <deoreameya@gmail.com>
2025-02-05 11:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const yesButton = document.getElementById("yesButton");
+    const noButton = document.getElementById("noButton");
+    const message = document.getElementById("responseMessage");
+    const container = document.getElementById("animationContainer");
+
+    let yesSize = 20; // Starting font size for "Yes" button
+
+    yesButton.addEventListener("click", () => {
+        message.innerText = "Yippppeeee can't wait to meet you again ❤️ Cya soon";
+        showAnimation();
+    });
+
+    noButton.addEventListener("click", () => {
+        yesSize += 10; // Increase size
+        yesButton.style.fontSize = `${yesSize}px`;
+
+        if (yesSize >= 100) { // When it gets big enough, make "No" disappear
+            noButton.style.display = "none";
+            message.innerText = "Cmon don't make me sad now. There's only one right choice 😉";
+        }
+    });
+
+    function showAnimation() {
+        container.innerHTML = ""; // Clear previous animations
+
+        const flowerImg = document.createElement("img");
+        flowerImg.src = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2NpajZ4a3pxeDZ1MjEwNTMzZnVpcHlwdjkzYWRmZWdhcnVxMjgxMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mBGcf7ReM5FHVCSyHT/giphy.gif";
+        flowerImg.classList.add("flower");
+
+        const bearImg = document.createElement("img");
+        bearImg.src = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTlhcTduNmtldzVhaHEzNHplM3BqdnIwcXZwenVzMHZjbTZ6a292bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tpVKvAabWt3G5csMkT/giphy.gif";
+        bearImg.classList.add("bear");
+
+        container.appendChild(flowerImg);
+        container.appendChild(bearImg);
+    }
+});
